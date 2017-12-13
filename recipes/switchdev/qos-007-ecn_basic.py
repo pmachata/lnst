8b7eadb738371b691af081e7801491df16d44822
Nogah Frankel <nogahf@mellanox.com>
2017-12-13 16:10:48 +0200

    recipes: switchdev: Basic RED-ECN test
    Add a basic test for RED-ECN. It is very similar to the basic RED test,
    but with some significant changes.
    Packets are not dropped but marked. So the backlog can and should go over
    the step limit in high rate. Pdrops are allowed in high rate and are
    required in very high rate. For low rate we can have the limit on the
    backlog size to be much closer to the step function.
    Another difference is that checking that ECN was turned off must be checked
    on the receiving host.
    
    Signed-off-by: Nogah Frankel <nogahf@mellanox.com>
    Reviewed-by: Petr Machata <petrm@mellanox.com>

---
+++ recipes/switchdev/qos-007-ecn_basic.py
@@ -0,0 +1,59 @@
+"""
+Copyright 2017 Mellanox Technologies. All rights reserved.
+Licensed under the GNU General Public License, version 2 as
+published by the Free Software Foundation; see COPYING for details.
+"""
+
+__author__ = """
+nogahf@mellanox.com (Nogah Frankel)
+"""
+
+from lnst.Controller.Task import ctl
+from TestLib import TestLib
+from random import randint
+from time import sleep
+import logging
+from RedTestLib import RedTestLib
+
+def run_actual_test(red_test):
+    red_test.set_red(1000000, 1000001, ecn = True)
+
+    low_ecn_res = red_test.tune_low_rate(0.95)
+    red_test.check_ecn_low(low_ecn_res)
+
+    high_ecn_res = red_test.tune_high_rate(1.1)
+    red_test.check_ecn_high(high_ecn_res)
+
+    red_test.set_higher_rate()
+    red_test.set_higher_rate()
+    very_high_ecn_res = red_test.send_traffic()
+    red_test.check_ecn_very_high(very_high_ecn_res, high_ecn_res)
+
+    red_test.set_no_red()
+    red_test.check_no_ecn()
+
+def do_task(ctl, hosts, ifaces, aliases):
+    m1, m2, sw = hosts
+    m1_if1, m2_if1, sw_if1, sw_if2 = ifaces
+
+    m1_if1.reset(ip=["192.168.101.10/24", "2002::1/64"])
+    m2_if1.reset(ip=["192.168.101.11/24", "2002::2/64"])
+
+    tl = TestLib(ctl, aliases)
+    red_test = RedTestLib(tl, sw, {sw_if1:m1_if1, sw_if2:m2_if1})
+    red_test.create_bottleneck(aliases)
+    sleep(30)
+    try:
+        run_actual_test(red_test)
+    except Exception as e:
+        logging.error("Test run failed")
+        tl.custom(sw, "Basic RED-ECN test", "Failed because of %s" % e)
+
+do_task(ctl, [ctl.get_host("machine1"),
+              ctl.get_host("machine2"),
+              ctl.get_host("switch")],
+        [ctl.get_host("machine1").get_interface("if1"),
+         ctl.get_host("machine2").get_interface("if1"),
+         ctl.get_host("switch").get_interface("if1"),
+         ctl.get_host("switch").get_interface("if2")],
+        ctl.get_aliases())
