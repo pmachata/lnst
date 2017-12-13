17bfb6d87d7657207196376940e303a9d5e4409f
Nogah Frankel <nogahf@mellanox.com>
2017-12-13 16:10:43 +0200

    recipes: switchdev: RedTestLib: Send traffic and collect stats
    Send traffic via the RED qdisc and collect statistics.
    Return the traffic run results in a new container class.
    
    Signed-off-by: Nogah Frankel <nogahf@mellanox.com>
    Reviewed-by: Petr Machata <petrm@mellanox.com>

--- recipes/switchdev/RedTestLib.py
+++ recipes/switchdev/RedTestLib.py
@@ -8,7 +8,19 @@ __author__ = """
 nogahf@mellanox.com (Nogah Frankel)
 """
 
+from time import sleep
 import logging
+
+# Merely a container for a run results
+# Holds:
+#   stats: The last red stats taken in the run, or, for none red runs, some
+#   minimal data from the normal stats.
+#   backlogs: A list of the backlogs sizes during the runs. Filtered so a
+#   backlog won't be counted if there was no TX in the time it was taken.
+class RedTestResults:
+    def __init__(self, red_stats, backlogs):
+        self.stats = red_stats
+        self.backlogs = backlogs
 
 class RedTestLib:
     def __init__(self, tl, switch, links):
@@ -53,4 +65,37 @@ class RedTestLib:
         self.min = 0
         self.max = 0
         self.egress_port.unset_qdisc_red()
+
+    # Send traffic in the set rate.
+    # If RED is set, collect res stats while the traffic is being sent.
+    # Collect the backlog size for every time there is a traffic passing via the
+    # red qdisc, indicated by change in the TX counter.
+    def send_traffic(self, is_red = True):
+        rx_before = self.ingress_port.link_stats()["rx_packets"]
+        tx_before = self.egress_port.link_stats()["tx_packets"]
+        pkt_size = self.links[self.ingress_port].get_mtu()
+
+        if is_red: # if RED is enabled
+            self.egress_port.collect_qdisc_red_stats()
+
+        self.tl.pktgen(self.links[self.ingress_port],
+                       self.links[self.egress_port], pkt_size, count=10**6,
+                       rate=str(self.rate)+"M", tos=self.tos)
+        sleep(3)
+
+        rx_after = self.ingress_port.link_stats()["rx_packets"]
+        tx_after = self.egress_port.link_stats()["tx_packets"]
+        tx_packets = tx_after - tx_before
+        rx_packets = rx_after - rx_before
+        drops = rx_packets - tx_packets
+
+        if is_red:
+            backlog, stats = self.egress_port.stop_collecting_qdisc_red_stats()
+            results = RedTestResults(stats, backlog)
+        else:
+            stats = {"drops": drops, "tx_packets": tx_packets}
+            results = RedTestResults(stats, [])
+
+        logging.info("Sent %d packets, dropped %d packets" % (tx_after, drops))
+        return results
 
