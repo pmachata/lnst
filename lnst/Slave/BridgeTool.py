pmachata/lnst

commit acfa1f8a1b7ddb22820f0c82e0d1e78b84add27c

Bridge: Add mdb add / delete functions
Add functions to add and delete mdb entries from a bridge.

Signed-off-by: Nogah Frankel <nogahf@mellanox.com>

lnst/Slave/BridgeTool.py
@@ -90,6 +90,18 @@ class BridgeTool:
                 br_fdb_info_list.append(br_fdb_info)
         return br_fdb_info_list
 
+    def _add_del_mdb(self, op, br_mdb_info):
+        print (op, self._dev_name, br_mdb_info["hwaddr"], br_mdb_info["group"])
+        cmd = "bridge mdb %s dev %s port %s grp %s" % \
+              (op, self._dev_name, br_mdb_info["hwaddr"], br_mdb_info["group"])
+        exec_cmd(cmd)
+
+    def add_mdb(self, br_mdb_info):
+        return self._add_del_mdb("add", br_mdb_info)
+
+    def del_mdb(self, br_mdb_info):
+        return self._add_del_mdb("del", br_mdb_info)
+
     def _set_link(self, attr, br_link_info):
         cmd = "bridge link set dev %s %s" % (self._dev_name, attr)
         if br_link_info["on"]:

lnst/Controller/Machine.py
@@ -1004,6 +1004,14 @@ class Interface(object):
     def get_br_fdbs(self):
         return self._machine._rpc_call_x(self._netns, "get_br_fdbs", self._id)
 
+    def add_br_mdb(self, br_mdb_info):
+        self._machine._rpc_call_x(self._netns, "add_br_mdb",
+                                  self._id, br_mdb_info)
+
+    def del_br_mdb(self, br_mdb_info):
+        self._machine._rpc_call_x(self._netns, "del_br_mdb",
+                                  self._id, br_mdb_info)
+
     def set_br_learning(self, br_learning_info):
         self._machine._rpc_call_x(self._netns, "set_br_learning", self._id,
                                   br_learning_info)

lnst/Controller/Task.py
@@ -784,6 +784,12 @@ class InterfaceAPI(object):
     def get_br_fdbs(self):
         return self._if.get_br_fdbs()
 
+    def add_br_mdb(_self, hwaddr, group):
+        _self._if.add_br_mdb({"hwaddr": hwaddr, "group": group})
+
+    def del_br_mdb(_self, hwaddr, group):
+        _self._if.del_br_mdb({"hwaddr": hwaddr, "group": group})
+
     def set_br_learning(_self, on=True, self=False, master=False):
         _self._if.set_br_learning({"on": on, "self": self, "master": master})
 

lnst/Slave/NetTestSlave.py
@@ -917,6 +917,24 @@ class SlaveMethods:
         brt = BridgeTool(dev.get_name())
         return brt.get_fdbs()
 
+    def add_br_mdb(self, if_id, br_mdb_info):
+        dev = self._if_manager.get_mapped_device(if_id)
+        if not dev:
+            logging.error("Device with id '%s' not found." % if_id)
+            return False
+        brt = BridgeTool(dev.get_name())
+        brt.add_mdb(br_mdb_info)
+        return True
+
+    def del_br_mdb(self, if_id, br_mdb_info):
+        dev = self._if_manager.get_mapped_device(if_id)
+        if not dev:
+            logging.error("Device with id '%s' not found." % if_id)
+            return False
+        brt = BridgeTool(dev.get_name())
+        brt.del_mdb(br_mdb_info)
+        return True
+
     def set_br_learning(self, if_id, br_learning_info):
         dev = self._if_manager.get_mapped_device(if_id)
         if not dev:

commit 44de1a3a6bf3b95d5bf956abd5825e3380e7957f

BridgeTool: Remove unneeded print
The print in _add_del_mdb doesn't give any info that running in debug mode
doesn't give and can be removed.

Fixes: acfa1f8a1b7d ("Bridge: Add mdb add / delete functions")
Signed-off-by: Nogah Frankel <nogahf@mellanox.com>

lnst/Slave/BridgeTool.py
@@ -91,7 +91,6 @@ class BridgeTool:
         return br_fdb_info_list
 
     def _add_del_mdb(self, op, br_mdb_info):
-        print (op, self._dev_name, br_mdb_info["hwaddr"], br_mdb_info["group"])
         cmd = "bridge mdb %s dev %s port %s grp %s" % \
               (op, self._dev_name, br_mdb_info["hwaddr"], br_mdb_info["group"])
         exec_cmd(cmd)