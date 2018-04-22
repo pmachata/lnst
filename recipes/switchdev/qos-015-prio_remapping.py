a297fa5b9b0737db2e7f03617cb103ad3be01c33
Nogah Frankel <nogahf@mellanox.com>
2018-04-22 11:49:36 +0300

    recipes: switchdev: Test PRIO remapping
    (Like test qos-012 but with remapping instead of changing TOS)
    
    Create a bottleneck for traffic by setting the ingress port to be in higher
    rate the then egress port.
    Set PRIO of 3 bands on the egress port.
    Send traffic in the middle band in a rate that will consume all the
    bandwidth.
    While this traffic run:
    * Send traffic in the lowest priority band and see that it doesn't being
      sent out.
    * Remap this traffic to the highest priority band and see that it is
      received.
    
    Signed-off-by: Nogah Frankel <nogahf@mellanox.com>
    Reviewed-by: Petr Machata <petrm@mellanox.com>

---
+++ recipes/switchdev/qos-015-prio_remapping.py
@@ -0,0 +1,49 @@
+"""
+Copyright 2017 Mellanox Technologies. All rights reserved.
+Licensed under the GNU General Public License, version 2 as
+published by the Free Software Foundation; see COPYING for details.
+"""
+
+__author__ = """
+nogahf@mellanox.com (Nogah Frankel)
+"""
+
+from lnst.Controller.Task import ctl
+from TestLib import TestLib
+from PrioTestLib import PrioTestLib
+from time import sleep
+import logging
+
+def do_task(ctl, hosts, ifaces, aliases):
+    m1, m2, sw = hosts
+    m1_if1, m2_if1, sw_if1, sw_if2 = ifaces
+
+    tl = TestLib(ctl, aliases)
+    prio_test = PrioTestLib(tl, sw, {sw_if1:m1_if1, sw_if2:m2_if1})
+    prio_test.create_bottleneck(aliases)
+    prio_test.set_prio(bands=3, priomap="2 2 2 2 1 1 1 1")
+    sleep(30)
+
+    logging.info("Start traffic in band 2")
+    prio_test.send_bg_traffic(6)
+    sleep(30)
+
+    logging.info("Send traffic in band 1 (shouldn't pass)")
+    prio_test.check_traffic_passes(0, False)
+
+    logging.info("Remap background traffic to band 0")
+    prio_test.set_prio(bands=3, priomap="0 0 0 0 1 1 1 1")
+    sleep(30)
+    logging.info("Send traffic in band 1 (should pass)")
+    prio_test.check_traffic_passes(0, True)
+
+    prio_test.stop_bg_traffic()
+
+do_task(ctl, [ctl.get_host("machine1"),
+              ctl.get_host("machine2"),
+              ctl.get_host("switch")],
+        [ctl.get_host("machine1").get_interface("if1"),
+         ctl.get_host("machine2").get_interface("if1"),
+         ctl.get_host("switch").get_interface("if1"),
+         ctl.get_host("switch").get_interface("if2")],
+        ctl.get_aliases())
