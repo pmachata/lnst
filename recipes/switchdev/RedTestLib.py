pmachata/lnst

commit 8b9fe622b653d8174dcde3bfa65f47e977b91582

recipes: switchdev: RedTestLib: Add backlog check
Check that the largest recorded backlog is not above the maximum limit in
high rate RED test.

Signed-off-by: Nogah Frankel <nogahf@mellanox.com>

recipes/switchdev/RedTestLib.py
@@ -249,6 +249,11 @@ class RedTestLib:
             msg = "Not enough early drops (%d)" % res.stats["early"]
         self.test_result(desc, msg)
 
+        if max(res.backlogs) > self.max * 1.2:
+            self.test_result("RED high rate - backlog over limit",
+                             "backlog was %d when max limit is %d" %
+                             (max(res.backlogs), self.max))
+
         self.check_no_pdrops("RED high rate - pdrops", res)
 
         desc = "RED high rate - throughput"

commit 1d7202e5b19604290b7cbeffeff6b3668697fcea

recipes: switchdev: Prevent error when no RED backlog
When no backlog exists, Python might provide the following traceback:

	File "/usr/share/lnst/recipes/switchdev/qos-018-prio_red_remapping_change_size.py",
	line 39, in do_task
		red_test.check_red_high(high_red_res)

	File "/usr/share/lnst/recipes/switchdev/RedTestLib.py", line 313, in
	check_red_high
		if max(res.backlogs) > self.max * 1.2:

	ValueError: max() arg is an empty sequence

Fixes: 8b9fe622b653 ("recipes: switchdev: RedTestLib: Add backlog check")
Signed-off-by: Yuval Mintz <yuvalm@mellanox.com>

recipes/switchdev/RedTestLib.py
@@ -310,7 +310,9 @@ class RedTestLib:
             msg = "Not enough early drops (%d)" % res.stats["early"]
         self.test_result(desc, msg)
 
-        if max(res.backlogs) > self.max * 1.2:
+        if not res.backlogs.size:
+            self.test_result("RED high rate no backlog results")
+        else if max(res.backlogs) > self.max * 1.2:
             self.test_result("RED high rate - backlog over limit",
                              "backlog was %d when max limit is %d" %
                              (max(res.backlogs), self.max))