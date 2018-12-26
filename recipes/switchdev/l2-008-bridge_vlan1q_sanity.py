f28e561d33c13e927024a36f8c79a398b2fa2363
Ido Schimmel <idosch@mellanox.com>
2018-12-26 14:48:28 +0200

    recipes: switchdev: Remove useless check
    The check only verifies iproute2's output which is not interesting and
    subject to changes.
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>

--- recipes/switchdev/l2-008-bridge_vlan1q_sanity.py
+++ recipes/switchdev/l2-008-bridge_vlan1q_sanity.py
@@ -16,19 +16,6 @@ def test_ip(major, minor):
     return ["192.168.10%d.%d/24" % (major, minor),
             "2002:%d::%d/64" % (major, minor)]
 
-def check_vlan(tl, iface, vlan_id, pvid=False, untagged = False):
-    vlans = iface.get_br_vlans()
-    err_msg = "vlan not found"
-    for vlan in vlans:
-        if vlan_id == vlan["vlan_id"]:
-            if pvid != vlan["pvid"]:
-                err_msg = "PVID is not as expected"
-            elif untagged != vlan["untagged"]:
-                err_msg = "Untagged is not as expected"
-            else:
-                err_msg = ""
-    tl.custom(iface.get_host(), "vlan creation verification", err_msg)
-
 def do_task(ctl, hosts, ifaces, aliases):
     m1, m2, sw = hosts
     m1_if1, m2_if1, sw_if1, sw_if2 = ifaces
@@ -56,23 +43,16 @@ def do_task(ctl, hosts, ifaces, aliases):
 
     tl = TestLib(ctl, aliases)
 
-    check_vlan(tl, sw_if1, 10)
-    check_vlan(tl, sw_if2, 10)
-    check_vlan(tl, sw_if1, 20)
-    check_vlan(tl, sw_if2, 20)
-
     tl.ping_simple(m1_if1, m2_if1)
     tl.ping_simple(m1_if1_10, m2_if1_10)
     tl.ping_simple(m1_if1_20, m2_if1_20)
     tl.ping_simple(m1_if1_30, m2_if1_30, fail_expected=True)
 
     sw_if1.add_br_vlan(500, pvid=True, untagged=True)
-    check_vlan(tl, sw_if1, 500, pvid=True, untagged=True)
     sleep(1)
     tl.ping_simple(m1_if1, m2_if1, fail_expected=True)
 
     sw_if2.add_br_vlan(500, pvid=True, untagged=True)
-    check_vlan(tl, sw_if2, 500, pvid=True, untagged=True)
     sleep(1)
     tl.ping_simple(m1_if1, m2_if1)
 
