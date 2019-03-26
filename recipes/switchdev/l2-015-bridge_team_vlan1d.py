pmachata/lnst

commit ed17b6c8064cbf53a46c2ea0f98c4661065af8d2

Add waiting to ports
Signed-off-by: Amit Cohen <amitc@mellanox.com>

recipes/switchdev/l2-015-bridge_team_vlan1d.py
@@ -46,9 +46,8 @@ def do_task(ctl, hosts, ifaces, aliases):
     sw_br3 = sw.create_bridge(slaves=[sw_lag1_20, sw_lag2_21],
                               options={"multicast_snooping": 0})
 
-    sleep(30)
-
     tl = TestLib(ctl, aliases)
+    tl.wait_for_if(ifaces)
 
     tl.ping_simple(m1_lag1, m2_lag1)
     tl.netperf_tcp(m1_lag1, m2_lag1)
@@ -71,7 +70,7 @@ def do_task(ctl, hosts, ifaces, aliases):
     sw_if1.reset(ip=test_ip(4, 1))
     m1_if1.reset(ip=test_ip(4, 2))
 
-    sleep(30)
+    tl.wait_for_if(ifaces)
 
     tl.ping_simple(sw_if1, m1_if1)
 
@@ -89,7 +88,7 @@ def do_task(ctl, hosts, ifaces, aliases):
 
     m1_lag1.slave_add(m1_if1.get_id())
 
-    sleep(30)
+    tl.wait_for_if(ifaces)
 
     tl.ping_simple(m1_lag1, m2_lag1)
     tl.netperf_tcp(m1_lag1, m2_lag1)

commit 47c1e199a9cd20f1e3eabfed6cb72582281f5451

recipes: switchdev: Only wait for relevant interfaces
Only `sw_if1` and `m1_if1` undergo a reset and take part in the slowpath
test.

We should therefore only wait for them.

Fixes: ed17b6c8064c ("Add waiting to ports")
Signed-off-by: Ido Schimmel <idosch@mellanox.com>

recipes/switchdev/l2-015-bridge_team_vlan1d.py
@@ -70,7 +70,7 @@ def do_task(ctl, hosts, ifaces, aliases):
     sw_if1.reset(ip=test_ip(4, 1))
     m1_if1.reset(ip=test_ip(4, 2))
 
-    tl.wait_for_if(ifaces)
+    tl.wait_for_if([sw_if1, m1_if1])
 
     tl.ping_simple(sw_if1, m1_if1)
 