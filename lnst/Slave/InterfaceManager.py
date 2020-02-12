pmachata/lnst

commit 0a51bd43afd0106129cd4699bbf3a46f3f9e30d1

TMP: lnst: Slave: InterfaceManager: WA for mlx5 rx-mcast counter bug
There is an open bug on mlx5 - Bug SW #1983694:
mlx5: rx-mcast doesn't increase in "ip -s link show $nic_name" output

It makes most of mr tests to fail.
The temporary solution is to run "ethtool -S.." before "ip -s..",
this command makes the rx-mcast to be updated.

Signed-off-by: Amit Cohen <amitc@mellanox.com>

lnst/Slave/InterfaceManager.py
@@ -700,6 +700,7 @@ class Device(object):
         stats = {"devname": self._name,
                  "hwaddr": self._hwaddr}
         try:
+            out, _ = exec_cmd("ethtool -S %s" % self._name)
             out, _ = exec_cmd("ip -s link show %s" % self._name)
         except:
             return {}

commit 8f88c9186cb65fa195aa6aaafa5b1ebdcac2ac8f

TMP: lnst: Slave: InterfaceManager: Fix WA
Not all devices implement ethtool operations and the call to 'ethtool
-S' can fail. Since the WA is only relevant for mlx5, make sure to only
issue the command for mlx5 netdevs.

Fixes: 0a51bd43afd0 ("TMP: lnst: Slave: InterfaceManager: WA for mlx5 rx-mcast counter bug")
Signed-off-by: Ido Schimmel <idosch@mellanox.com>

lnst/Slave/InterfaceManager.py
@@ -699,8 +699,9 @@ class Device(object):
     def link_stats(self):
         stats = {"devname": self._name,
                  "hwaddr": self._hwaddr}
-        try:
+        if self._driver is not None and 'mlx5' in self._driver:
             out, _ = exec_cmd("ethtool -S %s" % self._name)
+        try:
             out, _ = exec_cmd("ip -s link show %s" % self._name)
         except:
             return {}